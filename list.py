bc2684fc97d6e1ddd5e91ec5953920e3ca548951
Sherali Sayfiyev <153213135+Sherali1234567qwerty@users.noreply.github.com>
2023-12-14 09:56:48 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+mevalar = ["olma", "shaftoli", "tarvuz"]
+#print(mevalar[0].title())  
+#print(mevalar[2].lover())  
+#print(mevalar[1].upper())
+#print(mevalar[-1].capitalize())
+mevalar.append("o'rik")  #append()  metodi listning oxiriga element qushadi
+#print(mevalar)
+#mevalar.insert(0, "banan") #insert([n],element) metod n-chi indeksdan element qo'shadi 
+#del mevalar[1]  #del list[n] metod n-chi o'rindagi elementni o'chiradi
+#mevalar.remove("olma") #remove.list("element") listdan elementni o'chirib tashlaydi
+#mahsulotlar = mevalar.pop(1)
+#mahsulotlar = mevalar.pop(0)  #list.pop(index) index dagi elementni sug;urib oladi
+
+print(mevalar)
+
